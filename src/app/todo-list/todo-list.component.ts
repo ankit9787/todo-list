e4b014afc8e049977af37b16ce07f10c10cf1ea7
ankit9787 <ank.singh977@gmail.com>
2021-03-13 14:30:35 +0530

    added reset funtionality

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
   item_name: string;
@@ -15,15 +15,15 @@ export class TodoListComponent implements OnInit {
     this.itemsChecked.push('call repair man');
   }
 
-  add_item(itemName) {
-    this.items.push(itemName);
+  addItem(itemName) {
+    if (!(itemName === '')) this.items.push(itemName);
   }
 
   checkedItems(itemName) {
     this.itemsChecked.push(itemName);
   }
 
-  refresh_list(event, value) {
+  refreshList(event, value) {
     if (event.target.checked == true) {
       this.items = this.items.filter((item) => item != value);
       this.itemsChecked.push(value);
@@ -31,5 +31,10 @@ export class TodoListComponent implements OnInit {
       this.itemsChecked = this.itemsChecked.filter((item) => item != value);
       this.items.push(value);
     }
+  }
+
+  reset(){
+    this.items = [];
+    this.itemsChecked = [];
   }
 }
